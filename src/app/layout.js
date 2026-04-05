9b24be30b5f91c1d24d501733e2c2755cf12484e
ABHISHEK BHATIA <abhishekbhatia@ABHISHEKs-MacBook-Air.local>
2026-04-05 13:27:35 +0530

    add gtag done

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import "../../src/Common/styles/global.scss"
 import Header from "@/Common/Components/Header/Header";
 import Footer from "@/Common/Components/Footer/Footer";
+import Script from "next/script";
 export const dynamic = "force-dynamic";
 
 
@@ -15,7 +16,22 @@ const baiJamjuree = Bai_Jamjuree({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={baiJamjuree.variable}>
+          {/* Google Analytics */}
+          <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-W9DTRVJWC5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-W9DTRVJWC5');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
